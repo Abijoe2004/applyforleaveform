44bac48b0b6a7a1ff21861dbf1604033435da2a1
Abijoe2004 <abijoe202004@gmail.com>
2025-04-11 00:09:36 +0530

    added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { ApplyforleaveComponent } from './applyforleave/applyforleave.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [ApplyforleaveComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
---
+++ src/app/applyforleave/applyforleave.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-applyforleave',
+  imports: [],
+  templateUrl: './applyforleave.component.html',
+  styleUrl: './applyforleave.component.css'
+})
+export class ApplyforleaveComponent {
+
+}
